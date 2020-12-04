38731fb469f2c4c1f9721a596099d03d251af5c8
Ko Nagasawa <nagasawa.ko@gmail.com>
2020-12-04 21:22:27 +0100

    Update constant of api key

--- app/src/main/java/com/technology/mycow/kotlinpractice/ConstantCollection.kt
+++ app/src/main/java/com/technology/mycow/kotlinpractice/ConstantCollection.kt
@@ -10,6 +10,7 @@ class ConstantCollection {
         const val RETROFIT_BASE_URL: String = "https://api.themoviedb.org/3/"
         const val RETROFIT_POPULAR_MOVIE: String = "movie/popular"
         const val RETROFIT_QUERY_VALUE_API_KEY: String = "api_key"
+        const val RETROFIT_API_KEY: String = "f4b7e5ccf9606a0350a04eec5b343df1"
     }
 
 }
